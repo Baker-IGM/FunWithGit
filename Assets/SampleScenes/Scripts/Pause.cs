67776d0121b236de3b9a8b3728b5b2a3b6f9d3ca
Lei McKenna (RIT Student) <blm5582@ad.rit.edu>
2019-10-03 11:17:57 -0400

    Pause

---
+++ Assets/SampleScenes/Scripts/Pause.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private bool paused = false;
+    public GameObject menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
